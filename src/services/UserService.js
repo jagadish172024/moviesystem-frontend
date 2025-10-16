3ca8fc0bec1d3f0591954708ffbf352e68092798
jagadish172024 <jagadishelumalai1@gmail.com>
2025-10-16 15:22:03 +0530

    Update UserService.js

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -2,13 +2,14 @@ import axios from "axios";
 class UserService{
 
     register(user){
-        return axios.post("http://localhost:8080/api/user/registration", user);
+        return axios.post("https://moviesystem-backend.onrender.com/api/user/registration", user);
     }
 
     getAllUsers(){
-        return axios.get("http://localhost:8080/api/user/getAllUsers");
+        return axios.get("https://moviesystem-backend.onrender.com/api/user/getAllUsers");
     }
 
 }
+
 
 export default new UserService();
