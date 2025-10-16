cb95cbda0eaadcb3d713d16a26a1a6999d4893be
jagadish172024 <jagadishelumalai1@gmail.com>
2025-10-16 15:20:58 +0530

    Update API endpoints for login and logout

--- src/services/LoginService.js
+++ src/services/LoginService.js
@@ -3,12 +3,13 @@ import axios from 'axios';
 class LoginService{
 
     login(loginParams){
-        return axios.post("http://localhost:8080/api/login", loginParams);
+        return axios.post("https://moviesystem-backend.onrender.com/api/login", loginParams);
     }
 
     logout(){
-        return axios.get("http://localhost:8080/api/logout");
+        return axios.get("https://moviesystem-backend.onrender.com/api/logout");
     }
 
 }
+
 export default new LoginService();
